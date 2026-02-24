bdf0942523234577811f2ab83acefe87b40e4f26
Asif Shamim <asifshamim12816@gmail.com>
2026-02-24 16:02:57 +0530

    Initial 1.10.37

---
+++ frontend/components/Header.jsx
@@ -0,0 +1,77 @@
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
+import React from "react";
+import { Button } from "./ui/button";
+import Link from "next/link";
+import Image from "next/image";
+import { Cookie, Refrigerator } from "lucide-react";
+import UserDropdown from "./UserDropdown";
+import { checkUser } from "@/lib/checkUser";
+
+const Header = async () => {
+  const user = await checkUser();
+  return (
+    <header className="fixed top-0 w-full border-b border-stone-200 bg-stone-50/80 backdrop-blur-md z-50 supports-backdrop-filter:bg-stone-50/60">
+      <nav className="container mx-auto px-4 h-16 flex items-center justify-between">
+        <Link href={user ? "/dashboard" : "/"}>
+          <Image
+            src="/orange-logo.png"
+            alt="Servd Logo"
+            width={60}
+            height={60}
+            className="w-16"
+          />
+        </Link>
+
+        <div className="hidden md:flex items-center space-x-8 text-sm font-medium text-stone-600">
+          <Link
+            href="/recipes"
+            className="hover:text-orange-600 transition-colors flex gap-1.5 items-center"
+          >
+            <Cookie className="w-4 h-4" />
+            My Recipes
+          </Link>
+          <Link
+            href="/pantry"
+            className="hover:text-orange-600 transition-colors flex gap-1.5 items-center"
+          >
+            <Refrigerator className="w-4 h-4" />
+            My Pantry
+          </Link>
+        </div>
+        <div className="flex items-center space-x-4">
+          <SignedIn>
+            {/* How to cook */}
+
+            <UserDropdown />
+          </SignedIn>
+
+          <SignedOut>
+            <SignInButton mode="modal">
+              <Button
+                variant="ghost"
+                className="text-stone-600 hover:text-orange-600 hover:bg-orange-50 font-medium"
+              >
+                Sign In
+              </Button>
+            </SignInButton>
+
+            <SignUpButton mode="modal">
+              <Button variant="primary" className="rounded-full px-6">
+                Get Started
+              </Button>
+            </SignUpButton>
+          </SignedOut>
+          {/* Show the user button when the user is signed in */}
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
