869dac9f835081fc4ad30a322bd4d8561cb0d0c5
Asif Shamim <asifshamim12816@gmail.com>
2026-02-26 00:29:40 +0530

    update v2.16.30

---
+++ frontend/app/(main)/pantry/page.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Pantrypage = () => {
+  return (
+    <div className="min-h-screen bg-stone-50 pt-24 pb-16 px-4">
+        <div className="container mx-auto max-w-5xl">
+            <div className="mb-4">
+                 <div className="flex items-center justify-between mb-6">
+                    
+                 </div>
+            </div>
+        </div>
+    </div>
+  );
+};
+
+export default Pantrypage;
