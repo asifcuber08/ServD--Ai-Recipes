f2690f1418db2ff9894c02b61428e5db3e8a511e
Asif Shamim <asifshamim12816@gmail.com>
2026-03-18 00:21:45 +0530

    update v3.27.25

---
+++ frontend/app/(main)/pantry/recipes/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PantryRecipesPage = () => {
+  return (
+    <div className="min-h-screen bg-stone-50 pt-24 pb-16 px-4">
+      PantryRecipesPage
+    </div>
+  );
+};
+
+export default PantryRecipesPage;
